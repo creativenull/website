c179d5bdf25f31a75ebbbd1b2d1e78a2ab0c2c58
Arnold Chand <chand.arnold@hotmail.com>
2019-05-13 16:51:30 -0400

    add links to footer

--- blog/.vuepress/theme/components/Footer.vue
+++ blog/.vuepress/theme/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="tcn__footer flex items-center justify-center text-white bg-black">
-    <h6 class="footer__copyright text-xs">2019 Arnold Chand (thecreativenobody) | powered by <a class="footer__link underline" href="#">vuepress</a>, <a class="footer__link underline" href="#">tailwindcss</a></h6>
+    <h6 class="footer__copyright text-xs">2019 Arnold Chand (thecreativenobody) | powered by <a class="footer__link underline" target="_blank" href="https://vuepress.vuejs.org/">vuepress</a>, <a class="footer__link underline" target="_blank" href="https://tailwindcss.com/">tailwindcss</a></h6>
   </footer>
 </template>
 
