9ac2e71284679fee5f8239ce91a97e87c6abfbc4
Arnold Chand <chand.arnold@hotmail.com>
2019-06-07 17:30:10 -0400

    add resume link

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -3,14 +3,35 @@ module.exports = {
   description: "A blog on electronics, software and everything in-between.",
   themeConfig: {
     nav: [
-      { text: "About", link: "/about" },
-      { text: "Posts", link: "/posts/" },
-      { text: "Github", link: "https://github.com/thecreativenobody", target: "_blank" },
-      { text: "LinkedIn", link: "https://linkedin.com/in/arnoldjchand" },
+      {
+        text: "About",
+        link: "/about"
+      },
+      {
+        text: "Resume",
+        link: "/resume"
+      },
+      {
+        text: "Posts",
+        link: "/posts/"
+      },
+      {
+        text: "Github",
+        link: "https://github.com/thecreativenobody",
+        target: "_blank"
+      },
+      {
+        text: "LinkedIn",
+        link: "https://linkedin.com/in/arnoldjchand",
+        target: "_blank"
+      },
     ]
   },
   postcss: {
-    plugins: [require("tailwindcss"), require("autoprefixer")]
+    plugins: [
+      require("tailwindcss"),
+      require("autoprefixer"),
+    ]
   },
   plugins: [
     ["vuepress-plugin-container", {
