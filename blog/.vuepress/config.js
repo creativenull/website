7879bd8738d9a70886e5a5f946a91a61ba490c15
Arnold Chand <chand.arnold@hotmail.com>
2020-06-30 12:46:31 -0400

    feat: add new prop to

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,42 +1,43 @@
 module.exports = {
-  title: "CreativeNull",
-  description: "A blog on hardware, software, thoughts, etc.",
+  title: 'CreativeNull',
+  username: 'creativenull',
+  description: 'Some random engineering notes, blog, etc',
   themeConfig: {
     nav: [
       {
-        text: "Home",
-        link: "/",
+        text: 'Home',
+        link: '/',
       },
       {
-        text: "Resume",
-        link: "/resume",
+        text: 'Resume',
+        link: '/resume',
       },
       {
-        text: "Posts",
-        link: "/posts",
+        text: 'Posts',
+        link: '/posts',
       },
       {
-        text: "Github",
-        link: "https://github.com/creativenull",
+        text: 'Github',
+        link: `https://github.com/${this.username}`,
       },
     ],
   },
   postcss: {
     plugins: [
-      require("tailwindcss"),
-      require("autoprefixer"),
+      require('tailwindcss'),
+      require('autoprefixer'),
     ],
   },
   plugins: [
-    ["vuepress-plugin-container", {
-      type: "tip",
-      before: info => `<div class="tip_container font-open"><p class="tip_container__title font-bold">${info}</p>`,
-      after: "</div>",
+    ['vuepress-plugin-container', {
+      type: 'tip',
+      before: info => `<div class='tip_container font-open'><p class='tip_container__title font-bold'>${info}</p>`,
+      after: '</div>',
     }],
-    ["vuepress-plugin-container", {
-      type: "update",
-      before: () => `<div class="upd_container font-open"><p class="upd_container__title font-bold">Update</p>`,
-      after: "</div>",
+    ['vuepress-plugin-container', {
+      type: 'update',
+      before: () => `<div class='upd_container font-open'><p class='upd_container__title font-bold'>Update</p>`,
+      after: '</div>',
     }],
   ],
 };
